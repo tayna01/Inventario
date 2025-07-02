a97000af2ca8399207b186905883bae3b9d69018
Tayna <taynavicente2019@gmail.com>
2025-07-01 22:40:59 -0300

    fix; corrigido classe itemEletronico

--- src/model/ItemEletronico.java
+++ src/model/ItemEletronico.java
@@ -6,9 +6,39 @@ public class ItemEletronico extends ItemInventario<String>{
     private String voltagem;
     private String marca;
 
-    public ItemAdministrativo(String nome, double valorUnitario, boolean ativo, Categoria categoria, String identificador) {
+    public ItemEletronico(String nome, double valorUnitario, boolean ativo, Categoria categoria, String identificador) {
         super(nome, valorUnitario, ativo, categoria, identificador);
         this.voltagem = voltagem;
+        this.marca = marca;
+    }
+
+    @Override
+    public double calcularValorTotal(int quantidade){
+        return getValorUnitario() * quantidade;
+    }
+
+    @Override
+    public String gerarDescricaoDetalhada() {
+        return "Item Eletrônico: " + getNome() +
+                ", Marca: " + marca +
+                ", Voltagem: " + voltagem +
+                ", Valor Unitário: R$" + getValorUnitario();
+    }
+
+    //Getters e Setters
+    public String getVoltagem(){
+        return voltagem;
+    }
+
+    public void setVoltagem(String voltagem){
+        this.voltagem = voltagem;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public void setMarca(String marca){
         this.marca = marca;
     }
 }
